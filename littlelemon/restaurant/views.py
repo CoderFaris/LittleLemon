38e217d8b10368b8a554a8c1d942bf527f34bf3a
WyngeNotFoundWasTaken <farki.kadric@gmail.com>
2024-05-11 11:01:11 +0200

    adding a serializer and views

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer, UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from rest_framework import viewsets, permissions
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+
