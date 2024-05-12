aadd4909ea3ccf907db52aa3c3a8996b498402ef
WyngeNotFoundWasTaken <farki.kadric@gmail.com>
2024-05-12 19:12:45 +0200

    adjusting models, serializers and adding tests

--- littlelemon/restaurant/tests.py
+++ littlelemon/restaurant/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from restaurant.models import Menu
+from restaurant.serializers import MenuSerializer
+from decimal import Decimal
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+class MenuTest(TestCase):
+    def test_get_item(self):
+        item = Menu.objects.create(title="IceCream", price=Decimal('80'), inventory=100)
+        self.assertEqual(str(item), "IceCream : 80")
+
